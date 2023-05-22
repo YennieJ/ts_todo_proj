42e23eae834f354ac6e7d469f09a14951d11032b
yen <l.yennie.j@gmail.com>
2023-05-22 17:00:04 +0900

    싥행 주석

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
+////////// TodoItems //////////
 // import TodoConsole from "./view/TodoConsole";
 
 // const todoConsole = new TodoConsole();
 
 // todoConsole.promptUser();
 
-import UnionType from "./view/UnionType";
+////////// ========= //////////
 
-const sorter = new UnionType([10, -7, 22, 3, 15]);
+////////// UnionType //////////
+// import UnionType from "./view/UnionType";
 
-console.log(sorter.sort());
+// const sorter = new UnionType([10, -7, 22, 3, 15]);
 
-const sorterStr = new UnionType("Typescript");
-console.log(sorterStr.sort());
+// console.log(sorter.sort());
+
+// const sorterStr = new UnionType("Typescript");
+// console.log(sorterStr.sort());
+
+////////// ========= //////////
